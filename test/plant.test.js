4f3c70e2228f36da84138b97340f692c7bc83e4b
Chris Guidry <chris.guidry@ironoak.energy>
2016-11-21 10:09:00 -0500

    Converted tabs to spaces.

--- test/plant.test.js
+++ test/plant.test.js
@@ -11,18 +11,18 @@ chai.use(chaiHttp);
 
 describe('Plants', () => {
   describe('/GET plant', () => {
-	  it('it should GET all the [plants]', (done) => {
-		chai.request(server)
-	    .get('/api/plants')
-	    .end((err, res) => {
-		  	res.should.have.status(200);
-		  	res.body.should.be.a('array');
-		  	res.body.length.should.be.eql(10);
-		  	res.body[0].should.have.property('_id');
-		  	res.body[0].should.have.property('class');
-		  	res.body[0].should.have.property('name');
-	      done();
-	    });
-	  });
+      it('it should GET all the [plants]', (done) => {
+        chai.request(server)
+        .get('/api/plants')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(10);
+            res.body[0].should.have.property('_id');
+            res.body[0].should.have.property('class');
+            res.body[0].should.have.property('name');
+          done();
+        });
+      });
   });
 });
